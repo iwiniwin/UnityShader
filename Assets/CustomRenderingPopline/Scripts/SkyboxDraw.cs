9ffc72c764c653ff179ec5c92a2d3d410f383222
iwiniwin <iwiniwin@foxmail.com>
2020-06-15 00:31:02 +0800

    添加自定义渲染模型

---
+++ Assets/CustomRenderingPopline/Scripts/SkyboxDraw.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SkyboxDraw 
+{
+    private static Mesh m_Mesh;
+    public static Mesh fullScreenMesh{
+        get{
+            if(m_Mesh != null){
+                return m_Mesh;
+            }
+            m_Mesh = new Mesh();
+            m_Mesh.vertices = new Vector3[]{
+                new Vector3(-1, -1, 0),
+                new Vector3(-1, 1, 0),
+                new Vector3(1, 1, 0),
+                new Vector3(1, -1, 0),
+            };
+            m_Mesh.uv = new Vector2[]{
+                new Vector2(0, 1),
+                new Vector2(0, 0),
+                new Vector2(1, 0),
+                new Vector2(1, 1),
+            };
+            m_Mesh.SetIndices(new int[]{0, 1, 2, 3}, MeshTopology.Quads, 0);
+            return m_Mesh;
+        }
+    }
+
+    // 根据属性名称获取一个唯一ID
+    public static int _Corner = Shader.PropertyToID("_Corner");
+    public Material skyboxMaterial;
+    private static Vector4[] corners = new Vector4[4];
+    public void DrawSkybox(Camera cam, RenderBuffer colorBuffer, RenderBuffer depthBuffer){
+        // 将四个远裁剪面角传入到shader中
+        corners[0] = cam.ViewportToWorldPoint(new Vector3(0, 0, cam.farClipPlane));
+        corners[1] = cam.ViewportToWorldPoint(new Vector3(1, 0, cam.farClipPlane));
+        corners[2] = cam.ViewportToWorldPoint(new Vector3(0, 1, cam.farClipPlane));
+        corners[3] = cam.ViewportToWorldPoint(new Vector3(1, 1, cam.farClipPlane));
+        skyboxMaterial.SetVectorArray(_Corner, corners);
+        skyboxMaterial.SetPass(0);
+        Graphics.SetRenderTarget(colorBuffer, depthBuffer);
+        Graphics.DrawMeshNow(fullScreenMesh, Matrix4x4.identity);
+    }
+}
---
+++ Assets/CustomRenderingPopline/Scripts/Test.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public RenderTexture rt;
+    public RenderTexture depthTexture;
+
+    public Transform[] cubeTransforms;
+    public Mesh cubeMesh;
+    public Material pureColorMaterial;
+    public SkyboxDraw skyboxDraw;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rt = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.ARGBHalf, RenderTextureReadWrite.Linear);
+        depthTexture = new RenderTexture(Screen.width, Screen.height, 24, RenderTextureFormat.Depth, RenderTextureReadWrite.Linear);
+    }
+
+    private void OnPostRender(){
+        Camera cam = Camera.current;
+
+        // rt设置为当前的渲染目标
+        Graphics.SetRenderTarget(rt);
+
+        // 将手动创建的rt执行clear操作，使其变成一个白色的贴图
+        GL.Clear(true, true, Color.white);
+
+        // start drawcall
+        pureColorMaterial.color = new Color(0, 0.5f, 0.8f);
+        pureColorMaterial.SetPass(0);
+
+        // 使用同一个材质，绘制多个模型
+        foreach(Transform transform in cubeTransforms){
+            Graphics.DrawMeshNow(cubeMesh, transform.localToWorldMatrix);
+        }
+        
+        // end drawcall
+
+        skyboxDraw.DrawSkybox(cam, rt.colorBuffer, depthTexture.depthBuffer);
+
+        // 将rt绘制到cam.targetTexture，cam.targetTexture如果为null，则绘制到屏幕上
+        Graphics.Blit(rt, cam.targetTexture);
+    }
+}
